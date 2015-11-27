de97d6aab9456d1861f61accc17bedc4a829b599
Rafael Schmid <rafaelfschmid@gmail.com>
2015-11-27 12:31:24 -0300

    Refatoracao parte 1 classe host

--- Background/src/com/net/multiway/background/Main.java
+++ Background/src/com/net/multiway/background/Main.java
@@ -1,16 +1,19 @@
 package com.net.multiway.background;
 
+import com.net.multiway.background.data.Data0x1000;
 import com.net.multiway.background.model.Host;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
-	public static void main(String[] args) {
-	
-		
-		Host host = new Host("192.168.4.4", 5000);
-		host.conection();
-		
-
-	}
+    public static void main(String[] args) {
+        Host host = new Host("192.168.4.4", 5000);
+        host.initialize();
+        
+        Data0x1000 data = new Data0x1000(1, 0, 65.0f, 1, 1000, 1550, 0, 0, 15000, 1.4685f, 5.0f, 0);
+        host.connect(data);
+    }
 
 }
--- Background/src/com/net/multiway/background/model/Host.java
+++ Background/src/com/net/multiway/background/model/Host.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import com.net.multiway.background.data.Data;
 import com.net.multiway.background.data.Data0x1000;
 import com.net.multiway.background.data.Data0x1001;
 import com.net.multiway.background.data.Data0x1002;
@@ -18,126 +17,125 @@ import com.net.multiway.background.send.Send0x1001;
 import com.net.multiway.background.send.Send0x1002;
 import com.net.multiway.background.send.Send0x1004;
 import com.net.multiway.background.utils.Utils;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Host {
 
-	private int door;
-	private String ip;
+    private int door;
+    private String ip;
 
-	public Host(String i, int d) {
-		this.door = d;
-		this.ip = i;
-	}
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Socket client;
 
-	public int getDoor() {
-		return door;
-	}
+    public Host(String i, int d) {
+        this.door = d;
+        this.ip = i;
+    }
 
-	public void setDoor(int door) {
-		this.door = door;
-	}
+    public int getDoor() {
+        return door;
+    }
 
-	public String getIp() {
-		return ip;
-	}
+    public void setDoor(int door) {
+        this.door = door;
+    }
 
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
+    public String getIp() {
+        return ip;
+    }
 
-	private void sendAPackage(int code, DataOutputStream os, Data data) throws IOException {
-		
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
 
-		if (code == 0x10000000) {
-			Send0x1000 s = new Send0x1000(os, (Data0x1000) data);
-			s.sender();
-		} else if (code == 0x10000001) {
-			Send0x1001 s = new Send0x1001(os, Data0x1001.CANCEL_TEST);
-			s.sender();
-		} else if (code == 0x10000002) {
-			Send0x1002 s = new Send0x1002(os, (Data0x1002) data);
-			s.sender();
-		} else if (code == 0x10000004) {
-			Send0x1004 s = new Send0x1004(os);
-			s.sender();
-		}
+    public void initialize() {
+        try {
+            this.client = new Socket(this.ip, this.door);
+            System.out.println("O cliente se conectou ao OTDR!");
+            this.in = new DataInputStream(client.getInputStream());
+            this.out = new DataOutputStream(client.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(Host.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-		// os.close();
+    private void sendPackage(Data0x1000 data) throws IOException {
+        Send0x1000 s = new Send0x1000(this.out, data);
+        s.sender();
+    }
 
-	}
+    private void sendPackage(Data0x1001 data) throws IOException {
+        Send0x1001 s = new Send0x1001(this.out, data);
+        s.sender();
+    }
 
-	private void receiveAPackage(DataInputStream is) throws IOException {
+    private void sendPackage(Data0x1002 data) throws IOException {
+        Send0x1002 s = new Send0x1002(this.out, data);
+        s.sender();
+    }
 
-		byte[] b = new byte[60];
-		try {
+    private void sendPackage() throws IOException {
+        Send0x1004 s = new Send0x1004(this.out);
+        s.sender();
+    }
 
-			// ignorando bytes inuteis
-			is.read(b);
-			while (true) {
-				b = new byte[44];
-				// ignorando cabecario
-				is.read(b);
-				byte[] CMcode = new byte[4];
-				byte[] DataLen = new byte[4];
-				is.read(CMcode);
-				// System.out.println(Utils.byte4ToInt(DataLen));
-				is.read(DataLen);
-				// codigo de resposta padao
-				if (Utils.byte4ToInt(CMcode) == 0xA0000000) {
-					Receive0xA000 r = new Receive0xA000(is, Utils.byte4ToInt(DataLen));
-					r.parser();
-					break;
-				}
-				if (Utils.byte4ToInt(CMcode) == 0x90000001) {
-					Receive0x9001 r = new Receive0x9001(is);
-					r.parser();
-					break;
-				}
-				if (Utils.byte4ToInt(CMcode) == 0x90000000) {
-					Receive0x9000 r = new Receive0x9000(is);
-					r.parser();
-					break;
-				}
-			}
+    private void receivePackage() throws IOException {
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        byte[] b = new byte[60];
+        try {
+            // ignorando bytes inuteis
+            this.in.read(b);
 
-	}
+            b = new byte[44];
+            // ignorando cabecario
+            this.in.read(b);
+            byte[] CMcode = new byte[4];
+            byte[] DataLen = new byte[4];
+            this.in.read(CMcode);
+            // System.out.println(Utils.byte4ToInt(DataLen));
+            this.in.read(DataLen);
+            // codigo de resposta padao
+            if (Utils.byte4ToInt(CMcode) == 0xA0000000) {
+                Receive0xA000 r = new Receive0xA000(this.in, Utils.byte4ToInt(DataLen));
+                r.parser();
+            } else if (Utils.byte4ToInt(CMcode) == 0x90000001) {
+                Receive0x9001 r = new Receive0x9001(this.in);
+                r.parser();
+            } else if (Utils.byte4ToInt(CMcode) == 0x90000000) {
+                Receive0x9000 r = new Receive0x9000(this.in);
+                r.parser();
+            }
 
-	public void conection() {
-		try {
-			Socket client = new Socket(this.ip, this.door);
-			System.out.println("O cliente se conectou ao OTDR!");
-			DataInputStream is = null;
-			is = new DataInputStream(client.getInputStream());
-			DataOutputStream os = null;
-			os = new DataOutputStream(client.getOutputStream());
-			// BD
-			Data0x1000 data = new Data0x1000(1, 0, 65.0f, 1, 1000, 1550, 0, 0, 15000, 1.4685f, 5.0f, 0);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-			sendAPackage(0x10000000, os, data);
-			for (int i = 0; i < 10; i++) {
-				receiveAPackage(is);
+    }
 
-				sendAPackage(0x10000004, os, data);
+    public void connect(Data0x1000 data) {
+        try {
+            // BD
+            sendPackage(data);
+            for (int i = 0; i < 10; i++) {
+                receivePackage();
 
-				System.out.println(i + 1);
-			}
-			
+                sendPackage();
 
+                System.out.println(i + 1);
+            }
 
-			client.close();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            this.client.close();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	}
+    }
 
 }
