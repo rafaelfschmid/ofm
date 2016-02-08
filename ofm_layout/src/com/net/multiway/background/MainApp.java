f7735f1c4a9e9ec76f1ef5414a020a320ef15518
Phelipe <phelipefabres@gmail.com>
2016-02-08 15:39:57 -0200

    Classe DataParameters modificada e funcionando

--- ofm_layout/src/com/net/multiway/background/MainApp.java
+++ ofm_layout/src/com/net/multiway/background/MainApp.java
@@ -5,39 +5,22 @@
  */
 package com.net.multiway.background;
 
-import com.net.multiway.background.view.ConfigurationWindowController;
-import com.net.multiway.background.view.DeviceAddDialogController;
-import com.net.multiway.background.view.MonitorWindowController;
-import com.net.multiway.background.model.Device;
 import com.net.multiway.background.model.IController;
 import com.net.multiway.background.model.Mode;
-import com.net.multiway.background.model.Result;
 import com.net.multiway.background.model.View;
 import com.net.multiway.background.view.MainSceneController;
-import static com.sun.deploy.trace.Trace.flush;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.chart.XYChart;
-import javafx.scene.chart.XYChart.Series;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Node;
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
 import static javafx.application.Application.launch;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
@@ -50,142 +33,16 @@ public class MainApp extends Application {
 	private BorderPane rootLayout;
 	private MainSceneController rootController;
 
-	private List<Stage> listWindow = new ArrayList<>();
-
-	private ObservableList<Result> resultsData = FXCollections.observableArrayList();
-	private ObservableList<Device> devicesData = FXCollections.observableArrayList();
-	private ObservableList<XYChart.Series<Double, Double>> coordinatesData = FXCollections.observableArrayList();
 
 	public static MainApp getInstance() {
 		return instance;
 	}
 
 	public MainApp() {
-		resultsData.add(new Result(11, 21, 31, 1.1, 2.1, 3.1, 4.1));
-		resultsData.add(new Result(12, 22, 32, 1.2, 2.2, 3.2, 4.2));
-		resultsData.add(new Result(13, 23, 33, 1.3, 2.3, 3.3, 4.3));
-
-		Device x1 = new Device("192.168.4.2", "192.168.4.0", "192.168.4.1");
-		x1.setCoordinatesData(generateDataLineChart());
-		devicesData.add(x1);
-
-		Device x2 = new Device("192.168.4.3", "192.168.4.0", "192.168.4.1");
-		x2.setCoordinatesData(generateDataLineChart());
-		devicesData.add(x2);
-
-		Device x3 = new Device("192.168.4.4", "192.168.4.0", "192.168.4.1");
-		x3.setCoordinatesData(generateDataLineChart());
-		devicesData.add(x3);
-
-		Device x4 = new Device("192.168.4.5", "192.168.4.0", "192.168.4.1");
-		x4.setCoordinatesData(generateDataLineChart());
-		devicesData.add(x4);
-
-		Device x5 = new Device("192.168.4.6", "192.168.4.0", "192.168.4.1");
-		x5.setCoordinatesData(generateDataLineChart());
-		devicesData.add(x5);
-
-		Series<Double, Double> aSeries = new Series<>();
-		aSeries.getData().add(new XYChart.Data(1, 23));
-		aSeries.getData().add(new XYChart.Data(2, 14));
-		aSeries.getData().add(new XYChart.Data(3, 15));
-		aSeries.getData().add(new XYChart.Data(4, 24));
-		aSeries.getData().add(new XYChart.Data(5, 34));
-		aSeries.getData().add(new XYChart.Data(6, 36));
-
-		coordinatesData.addAll(aSeries);
+		
 	}
 
-	private ObservableList<XYChart.Series<Double, Double>> generateDataLineChart() {
-		ObservableList<XYChart.Series<Double, Double>> data = FXCollections.observableArrayList();
-		XYChart.Series<Double, Double> series1 = new XYChart.Series<>();
-		series1.getData().add(new XYChart.Data(1, Math.random() * 100 - 50));
-		series1.getData().add(new XYChart.Data(2, Math.random() * 100 - 50));
-		series1.getData().add(new XYChart.Data(3, Math.random() * 100 - 50));
-		series1.getData().add(new XYChart.Data(4, Math.random() * 100 - 50));
-		series1.getData().add(new XYChart.Data(5, Math.random() * 100 - 50));
-		series1.getData().add(new XYChart.Data(6, Math.random() * 100 - 50));
-		data.addAll(series1);
 
-		return data;
-	}
-
-	public ObservableList<Result> getResultsData() {
-		return resultsData;
-	}
-
-	public ObservableList<Device> getDevicesData() {
-		return devicesData;
-	}
-
-	public ObservableList<XYChart.Series<Double, Double>> getCoordinatesData() {
-		return coordinatesData;
-	}
-
-//    @Override
-//    public void start(Stage primaryStage) {
-//        this.primaryStage = primaryStage;
-//        this.primaryStage.setTitle("Optical Fiber Monitor");
-//
-//        //set the application icon
-//        //this.primaryStage.getIcons().add(new Image("file:resources/truck_red.png"));
-//   //     initMainScene();
-//
-//        openConfigurationWindow();
-//    }
-//
-//    /**
-//     * Inicializa o root layout (layout base).
-//     */
-//    public void initMainScene() {
-//        try {
-//            // Carrega o root layout do arquivo fxml.
-//            FXMLLoader loader = new FXMLLoader();
-//            loader.setLocation(MainApp.class.getResource("view/MainScene.fxml"));
-//
-//            //mainScene = (BorderPane) loader.load();
-//            rootLayout = (BorderPane) loader.load();
-//
-//            // Mostra a scene (cena) contendo oroot layout.
-//            Scene scene = new Scene(rootLayout);
-//            primaryStage.setScene(scene);
-//            primaryStage.show();
-//            //primaryStage.setMaximized(true);
-//
-//        } catch (IOException ex) {
-//            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-//
-//    /**
-//     * Mostra o person overview dentro do root layout.
-//     */
-//    public void openConfigurationWindow() {
-//        try {
-//          // Loads the root layout from fxml file
-//            FXMLLoader rootLoader = new FXMLLoader();
-//            rootLoader.setLocation(MainApp.class.getResource(View.MainScene.getResource()));
-//            rootLayout = (BorderPane) rootLoader.load();
-//
-//            // Gets RootLayoutController
-//            rootController = rootLoader.getController();
-//            rootController.prepareForm(null);
-//            rootController.prepareMenu(null);
-//
-//            // Loads welcome view
-//            FXMLLoader loader = new FXMLLoader();
-//            loader.setLocation(MainApp.class.getResource(View.ConfigurationWindow.getResource()));
-//            Node node = loader.load();
-//            IController controller = (IController) loader.getController();
-//            controller.prepareForm(null);
-//            controller.prepareMenu(null);
-//            rootController.setCenterController(controller);
-//      //      rootLayout.setCenter((AnchorPane) node);
-//
-//        } catch (IOException ex) {
-//            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
 	/**
 	 * Shows the stage with the Voting Process Insert View
 	 *
@@ -224,13 +81,13 @@ public class MainApp extends Application {
 			rootController.prepareForm(null);
 			rootController.prepareMenu(null);
 
-			// Loads welcome view
+			// Loads Configuration view
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource(View.ConfigurationWindow.getResource()));
 			Node node = loader.load();
 			IController controller = (IController) loader.getController();
-			controller.prepareForm(null);
-			controller.prepareMenu(null);
+			controller.prepareForm(Mode.VIEW);
+			controller.prepareMenu(Mode.VIEW);
 			rootController.setCenterController(controller);
 			rootLayout.setCenter((AnchorPane) node);
 		} catch (IOException ex) {
@@ -238,20 +95,7 @@ public class MainApp extends Application {
 		}
 	}
 
-	private int findWindow(Device device) {
-		for (int i = 0; i < listWindow.size(); i++) {
-			if (listWindow.get(i).getTitle() == device.getIp()) {
-				return i;
-			}
-		}
-
-		return -1;
-	}
-
-	public void closeWindow(Stage stage) {
-		listWindow.remove(stage);
-	}
-
+	
 	
 	/**
 	 * Abre uma janela para editar detalhes para o dispositivo especificado. Se
--- ofm_layout/src/com/net/multiway/background/view/ConfigurationWindowController.java
+++ ofm_layout/src/com/net/multiway/background/view/ConfigurationWindowController.java
@@ -6,6 +6,7 @@
 package com.net.multiway.background.view;
 
 import com.net.multiway.background.MainApp;
+import com.net.multiway.background.data.DataParameters;
 import com.net.multiway.background.model.Device;
 import com.net.multiway.background.model.IController;
 import com.net.multiway.background.model.Mode;
@@ -16,6 +17,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static javafx.application.Platform.exit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,6 +25,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
@@ -30,265 +33,256 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.util.Callback;
 
-
 /**
  * FXML Controller class
  *
  * @author rafael
  */
-public class ConfigurationWindowController implements Initializable,IController {//implements Initializable {
+public class ConfigurationWindowController implements Initializable, IController {//implements Initializable {
 
-    //device
-    @FXML
-    private ListView<Device> devicesList;
+	private DataParameters parameters;
+	//device
+	@FXML
+	private ListView<Device> devicesList;
 
-    // Gráfico
-    @FXML
-    private LineChart<Double, Double> grafico;
-    @FXML
-    private NumberAxis xAxis;
-    @FXML
-    private NumberAxis yAxis;
+	// Gráfico
+	@FXML
+	private LineChart<Double, Double> grafico;
+	@FXML
+	private NumberAxis xAxis;
+	@FXML
+	private NumberAxis yAxis;
 
-    //parameter
-    @FXML
-    private TextField measureModeField;
-    @FXML
-    private TextField optimizeModeField;
-    @FXML
-    private TextField reflectionThresholdField;
-    @FXML
-    private TextField enabledRefreshField;
-    @FXML
-    private TextField refreshCycleField;
-    @FXML
-    private TextField testWaveLengthField;
-    @FXML
-    private TextField measuringRangeOfTestField;
-    @FXML
-    private TextField testPulseWidthField;
-    @FXML
-    private TextField measuringTimeField;
-    @FXML
-    private TextField refractiveIndexField;
-    @FXML
-    private TextField endThresholdField;
-    @FXML
-    private TextField nonReflactionThresholdField;
+	//parameter
+	@FXML
+	private TextField measureModeField;
+	@FXML
+	private TextField optimizeModeField;
+	@FXML
+	private TextField reflectionThresholdField;
+	@FXML
+	private TextField enabledRefreshField;
+	@FXML
+	private TextField refreshCycleField;
+	@FXML
+	private TextField testWaveLengthField;
+	@FXML
+	private TextField measuringRangeOfTestField;
+	@FXML
+	private TextField testPulseWidthField;
+	@FXML
+	private TextField measuringTimeField;
+	@FXML
+	private TextField refractiveIndexField;
+	@FXML
+	private TextField endThresholdField;
+	@FXML
+	private TextField nonReflactionThresholdField;
 
-    //result
-    @FXML
-    private TableView<Result> resultTable;
-    @FXML
-    private TableColumn<Result, Integer> numeroColumn;
-    @FXML
-    private TableColumn<Result, Integer> typeColumn;
-    @FXML
-    private TableColumn<Result, Integer> distanceColumn;
-    @FXML
-    private TableColumn<Result, Double> insertLossColumn;
-    @FXML
-    private TableColumn<Result, Double> reflectLossColumn;
-    @FXML
-    private TableColumn<Result, Double> accumulationColumn;
-    @FXML
-    private TableColumn<Result, Double> attenuationCoefficientColumn;
+	//result
+	@FXML
+	private TableView<Result> resultTable;
+	@FXML
+	private TableColumn<Result, Integer> numeroColumn;
+	@FXML
+	private TableColumn<Result, Integer> typeColumn;
+	@FXML
+	private TableColumn<Result, Integer> distanceColumn;
+	@FXML
+	private TableColumn<Result, Double> insertLossColumn;
+	@FXML
+	private TableColumn<Result, Double> reflectLossColumn;
+	@FXML
+	private TableColumn<Result, Double> accumulationColumn;
+	@FXML
+	private TableColumn<Result, Double> attenuationCoefficientColumn;
+	@FXML
+	private Button buttonSave;
 
-    /**
-     * Initializes the controller class.
-     */
-    @FXML
-    public void initialize() {
-        devicesList.setCellFactory(new Callback<ListView<Device>, ListCell<Device>>() {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		parameters = new DataParameters();
 
-            @Override
-            public ListCell<Device> call(ListView<Device> p) {
+	}
 
-                ListCell<Device> cell = new ListCell<Device>() {
+	private void alertToSaveParameters() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Parametros não salvos");
+		alert.setHeaderText("O parâmetros não foram salvos.");
 
-                    @Override
-                    protected void updateItem(Device t, boolean bln) {
-                        super.updateItem(t, bln);
-                        if (t != null) {
-                            setText(t.getIp());
-                        }
-                    }
+		alert.showAndWait();
+	}
 
-                };
+	private void alertIncorrectTypeParameters(String ex) {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Tipo incorreto");
+		alert.setHeaderText("Por favor, verifique os parâmetros.");
+		alert.setContentText(ex);
 
-                return cell;
-            }
-        });
+		alert.showAndWait();
+	}
 
-        // Inicializa grafico
-        // Inicializa a tabela de parametros
-        measureModeField.setText("1");
-        optimizeModeField.setText("2");
-        reflectionThresholdField.setText("3.783");
-        enabledRefreshField.setText("4");
-        refreshCycleField.setText("5");
-        testWaveLengthField.setText("6");
-        measuringRangeOfTestField.setText("7");
-        testPulseWidthField.setText("8");
-        measuringTimeField.setText("9");
-        refractiveIndexField.setText("10.4898");
-        endThresholdField.setText("11.2672");
-        nonReflactionThresholdField.setText("12.3783");
+	private void alertDeviceSelection() {
+		// Nada selecionado.
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Nenhuma seleção");
+		alert.setHeaderText("Nenhum dispositivo foi selecionado");
+		alert.setContentText("Por favor, selecione um dispositivo.");
 
-        // Inicializa a tabela de resultados
-        numeroColumn.setCellValueFactory(cellData -> cellData.getValue().numberProperty());
-        typeColumn.setCellValueFactory(cellData -> cellData.getValue().distanceProperty());
-        distanceColumn.setCellValueFactory(cellData -> cellData.getValue().distanceProperty());
-        insertLossColumn.setCellValueFactory(cellData -> cellData.getValue().insertLossProperty());
-        reflectLossColumn.setCellValueFactory(cellData -> cellData.getValue().reflectLossProperty());
-        accumulationColumn.setCellValueFactory(cellData -> cellData.getValue().accumulationLossProperty());
-        attenuationCoefficientColumn.setCellValueFactory(cellData -> cellData.getValue().attenuationCoefficientProperty());
+		alert.showAndWait();
+	}
 
-        numeroColumn.setMinWidth(40);
-        typeColumn.setMinWidth(55);
-        distanceColumn.setMinWidth(90);
-        insertLossColumn.setMinWidth(105);
-        reflectLossColumn.setMinWidth(110);
-        accumulationColumn.setMinWidth(160);
-        attenuationCoefficientColumn.setMinWidth(190);
+	private boolean alertDeviceDeletion(String device) {
+		// Nada selecionado.
+		Alert alert = new Alert(Alert.AlertType.WARNING);
+		alert.setTitle("Exclusão de dispositivo");
+		alert.setHeaderText("Deseja excluir o dispositivo: " + device + "?");
 
-    }
+		alert.showAndWait();
 
+		if (alert.getResult() == null) {
+			return false;
+		}
 
-    private void alertToSaveParameters() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Parametros não salvos");
-        alert.setHeaderText("O parâmetros não foram salvos.");
+		return true;
+	}
 
-        alert.showAndWait();
-    }
+	/**
+	 * Chamado quando o usuário clica no botão delete.
+	 */
+	@FXML
+	private void onHandleDeleteDevice() {
 
-    private void alertIncorrectTypeParameters(String ex) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Tipo incorreto");
-        alert.setHeaderText("Por favor, verifique os parâmetros.");
-        alert.setContentText(ex);
+		int selectedIndex = devicesList.getSelectionModel().getSelectedIndex();
 
-        alert.showAndWait();
-    }
+		if (selectedIndex >= 0) {
+			if (alertDeviceDeletion(devicesList.getSelectionModel().getSelectedItem().getIp())) {
+				devicesList.getItems().remove(selectedIndex);
+			}
 
-    private void alertDeviceSelection() {
-        // Nada selecionado.
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Nenhuma seleção");
-        alert.setHeaderText("Nenhum dispositivo foi selecionado");
-        alert.setContentText("Por favor, selecione um dispositivo.");
+		} else {
+			alertDeviceSelection();
+		}
 
-        alert.showAndWait();
-    }
+		devicesList.setCellFactory(new Callback<ListView<Device>, ListCell<Device>>() {
 
-    private boolean alertDeviceDeletion(String device) {
-        // Nada selecionado.
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Exclusão de dispositivo");
-        alert.setHeaderText("Deseja excluir o dispositivo: " + device + "?");
+			@Override
+			public ListCell<Device> call(ListView<Device> p) {
 
-        alert.showAndWait();
+				ListCell<Device> cell = new ListCell<Device>() {
 
-        if (alert.getResult() == null) {
-            return false;
-        }
+					@Override
+					protected void updateItem(Device t, boolean bln) {
+						super.updateItem(t, bln);
+						if (t != null) {
+							setText(t.getIp());
+						}
+					}
 
-        return true;
-    }
+				};
 
-    /**
-     * Chamado quando o usuário clica no botão delete.
-     */
-    @FXML
-    private void onHandleDeleteDevice() {
+				return cell;
+			}
+		});
+	}
 
-        int selectedIndex = devicesList.getSelectionModel().getSelectedIndex();
-
-        if (selectedIndex >= 0) {
-            if (alertDeviceDeletion(devicesList.getSelectionModel().getSelectedItem().getIp())) {
-                devicesList.getItems().remove(selectedIndex);
-            }
-
-        } else {
-            alertDeviceSelection();
-        }
-
-        devicesList.setCellFactory(new Callback<ListView<Device>, ListCell<Device>>() {
-
-            @Override
-            public ListCell<Device> call(ListView<Device> p) {
-
-                ListCell<Device> cell = new ListCell<Device>() {
-
-                    @Override
-                    protected void updateItem(Device t, boolean bln) {
-                        super.updateItem(t, bln);
-                        if (t != null) {
-                            setText(t.getIp());
-                        }
-                    }
-
-                };
-
-                return cell;
-            }
-        });
-    }
-
-    /**
-     * Chamado quando o usuário clica no botão delete.
-     */
-    @FXML
-    private void onHandleAddDevice() {
+	/**
+	 * Chamado quando o usuário clica no botão delete.
+	 */
+	@FXML
+	private void onHandleAddDevice() {
 //        Device tempDevice = new Device();
 //        boolean okClicked = MainApp.getInstance().openDeviceDialog(tempDevice);
 //        if (okClicked) {
 //            MainApp.getInstance().getDevicesData().add(tempDevice);
 //        }
-    }
+	}
+	private void alertIncorrectField(String text) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Campo Incorreto");
+        alert.setHeaderText("Por favor, verifique o campo " + text + ".");
 
-    @FXML
-    private void onHandleEditParameters() {
-       
-    }
-
-    @FXML
-    private void onHandleSaveParameters() {
-        
-    }
-
-    @FXML
-    private void onHandleExecute() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Implementar");
-        alert.setHeaderText("//TO DO");
         alert.showAndWait();
     }
 
-    @FXML
-    private void onHandleExport() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Implementar");
-        alert.setHeaderText("//TO DO");
-        alert.showAndWait();
-    }
+	@FXML
+	private void onHandleEditParameters() {
+		prepareForm(Mode.EDIT);
 
-    @FXML
-    private void onHandleSetReference() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Implementar");
-        alert.setHeaderText("//TO DO");
-        alert.showAndWait();
-    }
+	}
 
-    @FXML
-    private void onHandleChangeToMonitor() throws IOException {
+	@FXML
+	private void onHandleSaveParameters() {
+		if (measureModeField.getText().isEmpty()) {
+			alertIncorrectField("Measure Mode");
+		} else if (optimizeModeField.getText().isEmpty()) {
+			alertIncorrectField("Optimize Mode");
+		} else if (reflectionThresholdField.getText().isEmpty()) {
+			alertIncorrectField("Reflection Threshold");
+		} else if (enabledRefreshField.getText().isEmpty()) {
+			alertIncorrectField("Enabled Refresh");
+		} else if (refreshCycleField.getText().isEmpty()) {
+			alertIncorrectField("Refresh Cycle");
+		} else if (testWaveLengthField.getText().isEmpty()) {
+			alertIncorrectField("Test Wave Length");
+		} else if (measuringRangeOfTestField.getText().isEmpty()) {
+			alertIncorrectField("Measuring Range Of Test");
+		} else if (testPulseWidthField.getText().isEmpty()) {
+			alertIncorrectField("Test Pulse Width");
+		} else if (measuringTimeField.getText().isEmpty()) {
+			alertIncorrectField("Measuring Time");
+		} else if (refractiveIndexField.getText().isEmpty()) {
+			alertIncorrectField("Refractive Index");
+		} else if (endThresholdField.getText().isEmpty()) {
+			alertIncorrectField("End Threshold");
+		} else if (nonReflactionThresholdField.getText().isEmpty()) {
+			alertIncorrectField("Non Reflaction Threshold");
+
+		}
+		else {
+			parameters.setMeasureMode(Integer.parseInt(measureModeField.getText()));
+			parameters.setOptimizeMode(Integer.parseInt(optimizeModeField.getText()));
+			parameters.setReflectionThreshold(Float.parseFloat(reflectionThresholdField.getText()));
+			parameters.setEnabledRefresh(Integer.parseInt(enabledRefreshField.getText()));
+			parameters.setRefreshCycle(Integer.parseInt(refreshCycleField.getText()));
+			parameters.setTestWaveLength(Integer.parseInt(testWaveLengthField.getText()));
+			parameters.setMeasuringRangeOfTest(Integer.parseInt(measuringRangeOfTestField.getText()));
+			parameters.setTestPulseWidth(Integer.parseInt(testPulseWidthField.getText()));
+			parameters.setMeasuringTime(Integer.parseInt(measuringTimeField.getText()));
+			parameters.setRefractiveIndex(Float.parseFloat(refractiveIndexField.getText()));
+			parameters.setEndThreshold(Float.parseFloat(endThresholdField.getText()));
+			parameters.setNonReflactionThreshold(Float.parseFloat(nonReflactionThresholdField.getText()));
+			prepareForm(Mode.VIEW);
+		}
+	}
+
+	@FXML
+	private void onHandleExecute() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Implementar");
+		alert.setHeaderText("//TO DO");
+		alert.showAndWait();
+	}
+
+	@FXML
+	private void onHandleExport() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Implementar");
+		alert.setHeaderText("//TO DO");
+		alert.showAndWait();
+	}
+
+	@FXML
+	private void onHandleSetReference() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Implementar");
+		alert.setHeaderText("//TO DO");
+		alert.showAndWait();
+	}
+
+	@FXML
+	private void onHandleChangeToMonitor() throws IOException {
 		MainApp.getInstance().showView(View.MonitorWindow, Mode.VIEW);
-    }
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
 	}
 
 	@Override
@@ -298,7 +292,39 @@ public class ConfigurationWindowController implements Initializable,IController 
 
 	@Override
 	public void prepareForm(Mode mode) {
+		switch (mode) {
+			case VIEW:
+				measureModeField.setDisable(true);
+				optimizeModeField.setDisable(true);
+				reflectionThresholdField.setDisable(true);
+				enabledRefreshField.setDisable(true);
+				refreshCycleField.setDisable(true);
+				testWaveLengthField.setDisable(true);
+				measuringRangeOfTestField.setDisable(true);
+				testPulseWidthField.setDisable(true);
+				measuringTimeField.setDisable(true);
+				refractiveIndexField.setDisable(true);
+				endThresholdField.setDisable(true);
+				nonReflactionThresholdField.setDisable(true);
+				buttonSave.setDisable(true);
+				break;
+			case EDIT:
+				measureModeField.setDisable(false);
+				optimizeModeField.setDisable(false);
+				reflectionThresholdField.setDisable(false);
+				enabledRefreshField.setDisable(false);
+				refreshCycleField.setDisable(false);
+				testWaveLengthField.setDisable(false);
+				measuringRangeOfTestField.setDisable(false);
+				testPulseWidthField.setDisable(false);
+				measuringTimeField.setDisable(false);
+				refractiveIndexField.setDisable(false);
+				endThresholdField.setDisable(false);
+				nonReflactionThresholdField.setDisable(false);
+				buttonSave.setDisable(false);
 
+				break;
+		}
 	}
 
 	@Override
